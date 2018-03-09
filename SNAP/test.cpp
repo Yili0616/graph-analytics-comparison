084e04999db127df3921da0228ad39e205784bdf
Yili0616 <yilihui@gatech.edu>
2018-03-09 11:15:37 -0500

    snap code added
    snap

---
+++ SNAP/test.cpp
@@ -0,0 +1,54 @@
+#include <ctime>
+#include <cstdlib>
+#include <typeinfo>
+#include "Snap.h"
+
+
+int main(int argc, char* argv[]) {
+  const clock_t begin_time = clock();
+  PUNGraph Graph = TSnap::LoadEdgeList<PUNGraph>("cit-Patents.txt",0,1);
+  printf("time for Read a graph:");
+  printf("%g\n", float(clock()-begin_time)/CLOCKS_PER_SEC);
+  TIntPrV count;
+  printf("number of nodes: %i\n", Graph->GetNodes());
+  printf("number of edges: %d\n", Graph->GetEdges());
+
+  const clock_t begin_time2 = clock();
+  TSnap::GetWccSzCnt(Graph, count);
+  printf("time for WCC:");
+  printf("%g\n", float(clock()-begin_time2)/CLOCKS_PER_SEC);
+
+  const clock_t begin_time3 = clock();
+  TIntFltH PRankH;
+  TSnap::GetPageRank(Graph, PRankH, 0.85, 0.0001, 10);
+  printf("time for PR:");
+  printf("%g\n", float(clock()-begin_time3)/CLOCKS_PER_SEC);
+
+  PUNGraph UGraph = TSnap::ConvertGraph<PUNGraph>(Graph);
+  const clock_t begin_time4 = clock();
+  TIntFltH CcfH;
+  TSnap::GetNodeClustCf(UGraph, CcfH);
+  printf("time for Clustering Coefficient:");
+  printf("%g\n", float(clock()-begin_time4)/CLOCKS_PER_SEC);
+
+  const clock_t begin_time5 = clock();
+  TKCore<PUNGraph> KCore(UGraph);
+  printf("3-core:");
+  printf("%g\n", float(clock()-begin_time5)/CLOCKS_PER_SEC);
+
+
+ 
+  const clock_t begin_time6 = clock();
+  printf("Does the edge exist? ");
+  printf(Graph->IsEdge(Graph->GetRndNId(), Graph->GetRndNId())?"true\n":"false\n");
+  printf("Time for test existence of edge");
+  printf("%g\n", float(clock()-begin_time6)/CLOCKS_PER_SEC);
+  
+
+
+
+
+
+
+
+}
